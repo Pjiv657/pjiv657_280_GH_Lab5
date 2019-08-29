f922255738ebe3115a119872f90260ebbf9370a0
pjiv657 <pjiv657@auckalnduni.ac.nz>
2019-08-29 16:54:58 +1200

    Re-fail

--- pjiv657_280_Lab2_in_Lab5/maths.py
+++ pjiv657_280_Lab2_in_Lab5/maths.py
@@ -42,7 +42,4 @@ def convert_base(num, n):
     return new_num_string
 
 def factorial(num):
-    fact = 1
-    for x in range (1,num + 1):
-        fact = fact * x
-    return fact       
+    None
